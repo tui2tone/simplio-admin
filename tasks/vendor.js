5c657da2575773f5f5610232b017e0bb94ad0938
tui2tone <tui2tone@gmail.com>
2016-08-12 14:49:48 +0700

    add elements

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -2,11 +2,24 @@
 
 import gulp from "gulp";
 
-const libs = [ 'bower_components/chartist-plugin-pointlabels/dist/chartist-plugin-pointlabels.min.js']
+const js_libs = [ 
+  'bower_components/chartist-plugin-pointlabels/dist/chartist-plugin-pointlabels.min.js'
+]
 
-gulp.task('vendor', ['vendor:js']);
+const css_libs = [
+  'bower_components/bootstrap/dist/css/bootstrap.min.css',
+  'bower_components/font-awesome/css/font-awesome.min.css',
+  'bower_components/chartist/dist/chartist.min.css'
+]
+
+gulp.task('vendor', ['vendor:js', 'vendor:css']);
 
 gulp.task('vendor:js', function () {
-  return gulp.src(libs)
+  return gulp.src(js_libs)
     .pipe(gulp.dest('temp/vendor/js'))
+});
+
+gulp.task('vendor:css', function () {
+  return gulp.src(css_libs)
+    .pipe(gulp.dest('temp/vendor/css'))
 });
